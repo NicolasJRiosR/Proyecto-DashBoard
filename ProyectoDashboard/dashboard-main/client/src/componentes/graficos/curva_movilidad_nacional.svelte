a5b1b679f24252ce8b9d4c137d4298e3438c9ccc
Miren <mirenmuu@gmail.com>
2025-11-25 10:04:17 +0100

    Avances en curva_movilidad_nacional

--- ProyectoDashboard/dashboard-main/client/src/componentes/graficos/curva_movilidad_nacional.svelte
+++ ProyectoDashboard/dashboard-main/client/src/componentes/graficos/curva_movilidad_nacional.svelte
@@ -49,14 +49,15 @@
       .attr("transform", `translate(0,${height - margin.bottom})`)
       .call(d3.axisBottom(x))
       .selectAll("text")
-      .attr("class", "text-black dark:text-white");
+      .attr("fill", textColor);
 
     // Eje Y
     svg.append("g")
       .attr("transform", `translate(${margin.left},0)`)
       .call(d3.axisLeft(y))
       .selectAll("text")
-      .attr("class", "text-black dark:text-white");
+      .attr("fill", textColor);
+
 
 
     // Etiquetas adaptadas al modo claro/oscuro
